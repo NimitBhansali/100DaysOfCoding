e6d87715cb7e73c969e3b77b6e1e73d3bfd828ac
NimitBhansali <99709269+NimitBhansali@users.noreply.github.com>
2022-10-05 23:21:15 +0530

    Delete Day 1 directory

--- Day 1/Hello.java
+++
@@ -1,9 +0,0 @@
-// Print Hello world 1st problem on hacker rank
-
-public class Hello {
-
-    public static void main(String[] args) {
-        System.out.println("Hello, World.");
-        System.out.println("Hello, Java.");
-    }
-}
