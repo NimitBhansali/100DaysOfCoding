473a36561cd009d84a4e66ee26df8ee05ee8d07b
Nimit Bhansali <99709269+NimitBhansali@users.noreply.github.com>
2023-02-09 21:38:27 +0530

    Valid Parentheses from leetcode

---
+++ Day_97_Java(Valid Parentheses from k=leetcode)09-02-2023/Solution.java
@@ -0,0 +1,25 @@
+class Solution{
+public boolean isValid(String s) {
+        if(s.length() % 2 !=0)
+            return false;
+        Stack <Character> st =new Stack();
+        for(char c : s.toCharArray()){
+            if(c=='(' || c=='{' || c=='['){
+                st.push(c);
+            }
+            else if(c==')' && !st.isEmpty() && st.peek()=='('){
+                st.pop();
+            }
+            else if(c=='}' && !st.isEmpty() && st.peek()=='{'){
+                st.pop();
+            }
+            else if(c==']' && !st.isEmpty() && st.peek()=='['){
+                st.pop();
+            }
+            else{
+                return false;
+            } 
+        }
+        return st.isEmpty();
+    }
+}
